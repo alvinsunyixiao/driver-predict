c3d10ba46c5005a9a88fc4e2c6b9b04009d4b4e0
Alvin Sun <alvinsunyixiao@gmail.com>
2021-11-27 21:35:14 -0800

    change rnn dynamics wip

--- dnn/loss.py
+++ dnn/loss.py
@@ -7,46 +7,22 @@ class LossManager:
     def __init__(self, net: DPNet):
         self.net = net
 
-    def compute(self, y_proc, Py_proc, y, img_bhw3, state_b3):
-        y_proc_dist = tfp.distributions.MultivariateNormalTriL(
-            loc=y_proc, scale_tril=tf.linalg.cholesky(Py_proc))
+    def image_recon_loss(self, img_bhw3, img_recon_bhw3):
+        img_loss_bhw = tf.reduce_sum(tf.square(img_bhw3 - img_recon_bhw3), axis=-1)
+        return tf.reduce_mean(img_loss_bhw)
 
-        tf.print(y_proc_dist.stddev())
-
-        y_loss = self.latent_meas_loss(y, y_proc_dist)
-        img_loss, state_loss = self.decoded_loss(img_bhw3, state_b3, y, y_proc_dist)
-
-        return y_loss, img_loss, state_loss
-
-    def latent_meas_loss(self, y, y_proc_dist):
-        return -tf.reduce_mean(y_proc_dist.log_prob(y))
-
-    def decoded_loss(self, img_bhw3, state_b3, y, y_proc_dist):
-        y_proc_sample = y
-
-        # decoded image loss
-        img_code = y_proc_sample[:, :self.net.p.img_enc_units]
-        img_recon = self.net.img_decoder(img_code)
-        img_loss = tf.reduce_sum(tf.square(img_recon - img_bhw3), axis=-1)
-        img_loss = tf.reduce_mean(img_loss)
-
-        # decoded state loss
-        state_code = y_proc_sample[:, self.net.p.img_enc_units:]
-        state_recon = self.net.state_decoder(state_code)
-
+    def state_recon_loss(self, state_b3, state_recon_b3):
         # position
-        pos_recon = state_recon[:, :2]
-        pos = state_b3[:, :2]
-        pos_loss = tf.reduce_sum(tf.square(pos_recon - pos), axis=-1)
+        pos_recon_b2 = state_recon_b3[:, :2]
+        pos_b2 = state_b3[:, :2]
+        pos_loss_b = tf.reduce_sum(tf.square(pos_recon_b2 - pos_b2), axis=-1)
 
         # rotation
-        rot_recon = state_recon[:, 2]
-        rot = state_b3[:, 2]
-        rot_diff = rot_recon - rot
-        rot_diff = tf.atan2(tf.sin(rot_diff), tf.cos(rot_diff))
-        rot_loss = tf.square(rot_diff)
+        rot_recon_b = state_recon_b3[:, 2]
+        rot_b = state_b3[:, 2]
+        rot_diff_b = rot_recon_b - rot_b
+        rot_diff_b = tf.atan2(tf.sin(rot_diff_b), tf.cos(rot_diff_b))
+        rot_loss_b = tf.square(rot_diff_b)
 
-        state_loss = tf.reduce_mean((pos_loss + rot_loss))
-
-        return img_loss, state_loss
+        return tf.reduce_mean(poss_loss_b + rot_loss_b)
 
--- dnn/model.py
+++ dnn/model.py
@@ -13,7 +13,9 @@ class MLP(KL.Layer):
                  output_units: int,
                  hidden_units: int,
                  num_layers: int,
+                 has_bn: bool = False,
                  dropout: T.Optional[float] = None,
+                 last_activation: T.Optional[str] = None,
                  **kwargs):
         super(MLP, self).__init__(**kwargs)
         self.output_units = output_units
@@ -30,15 +32,20 @@ class MLP(KL.Layer):
             # fully connected layer
             layers.append(KL.Dense(
                 units=hidden_units,
-                activation="elu",
-                #kernel_regularizer=K.regularizers.l2(1e-4),
-                #bias_regularizer=K.regularizers.l2(1e-4),
             ))
+
+            # batchnorm
+            if has_bn:
+                layers.append(KL.BatchNormalization())
+
+            # activation
+            layers.append(KL.ReLU())
+
             # dropout
             if dropout is not None:
                 layers.append(KL.Dropout(dropout))
 
-        layers.append(KL.Dense(self.output_units))
+        layers.append(KL.Dense(self.output_units, activation=last_activation))
 
         self.f = K.Sequential(layers)
 
@@ -51,22 +58,20 @@ class ImageEncoder(KL.Layer):
         self.units = units
         self.dropout = dropout
 
-        reg = None #K.regularizers.l2(1e-4)
         self.cnn = K.Sequential([
-            KL.Conv2D(24, 7, 2, activation="elu", kernel_regularizer=reg, bias_regularizer=reg),
-            KL.Conv2D(24, 5, 1, activation="elu", kernel_regularizer=reg, bias_regularizer=reg),
-            KL.Conv2D(48, 5, 2, activation="elu", kernel_regularizer=reg, bias_regularizer=reg),
-            KL.Conv2D(48, 3, 1, activation="elu", kernel_regularizer=reg, bias_regularizer=reg),
-            KL.Conv2D(72, 3, 2, activation="elu", kernel_regularizer=reg, bias_regularizer=reg),
-            KL.Conv2D(72, 3, 1, activation="elu", kernel_regularizer=reg, bias_regularizer=reg),
-            KL.Conv2D(96, 3, 2, activation="elu", kernel_regularizer=reg, bias_regularizer=reg),
-            KL.Conv2D(96, 3, 1, activation="elu", kernel_regularizer=reg, bias_regularizer=reg),
-            KL.Conv2D(128, 3, 2, activation="elu", kernel_regularizer=reg, bias_regularizer=reg),
+            KL.Conv2D(24, 7, 2, "same", activation="relu"),
+            KL.Conv2D(48, 5, 2, "same", activation="relu"),
+            KL.Conv2D(96, 5, 2, "same", activation="relu"),
+            KL.Conv2D(144, 3, 2, "same", activation="relu"),
+            KL.Conv2D(144, 3, 1, "valid", activation="relu"),
+            KL.Conv2D(192, 3, 1, "valid", activation="relu"),
+            KL.Conv2D(192, 3, 1, "valid", activation="relu"),
+            KL.Conv2D(256, 3, 1, "valid", activation="relu"),
         ])
-        self.mlp = MLP(units, hidden_units=512, num_layers=1, dropout=dropout)
+        self.mlp = MLP(units, hidden_units=1024, num_layers=1, dropout=dropout)
 
     def call(self, x, training=None):
-        x = self.cnn(x)
+        x = self.cnn(x, training=training)
         x = KL.Flatten()(x)
         x = self.mlp(x, training=training)
 
@@ -78,103 +83,26 @@ class ImageDecoder(KL.Layer):
         self.feat_shape = feat_shape
         self.dropout = dropout
 
-        reg = None #K.regularizers.l2(1e-4)
-        self.mlp = MLP(math.prod(feat_shape), hidden_units=512, num_layers=1, dropout=dropout)
+        self.mlp = MLP(math.prod(feat_shape), hidden_units=1024, num_layers=1, dropout=dropout, last_activation="relu")
+        self.reshape = KL.Reshape(feat_shape)
         self.decnn = K.Sequential([
-            KL.Conv2DTranspose(96, 3, 2, activation="elu", output_padding=(0, 1),
-                               kernel_regularizer=reg, bias_regularizer=reg),
-            KL.Conv2DTranspose(96, 3, 1, activation="elu",
-                               kernel_regularizer=reg, bias_regularizer=reg),
-            KL.Conv2DTranspose(72, 3, 2, activation="elu", output_padding=1,
-                               kernel_regularizer=reg, bias_regularizer=reg),
-            KL.Conv2DTranspose(72, 3, 1, activation="elu",
-                               kernel_regularizer=reg, bias_regularizer=reg),
-            KL.Conv2DTranspose(48, 3, 2, activation="elu",
-                               kernel_regularizer=reg, bias_regularizer=reg),
-            KL.Conv2DTranspose(48, 3, 1, activation="elu",
-                               kernel_regularizer=reg, bias_regularizer=reg),
-            KL.Conv2DTranspose(24, 5, 2, activation="elu",
-                               kernel_regularizer=reg, bias_regularizer=reg),
-            KL.Conv2DTranspose(24, 5, 1, activation="elu",
-                               kernel_regularizer=reg, bias_regularizer=reg),
-            KL.Conv2DTranspose(3, 7, 2, activation=None,
-                               kernel_regularizer=reg, bias_regularizer=reg),
+            KL.Conv2DTranspose(192, 3, 1, "valid", activation="relu"),
+            KL.Conv2DTranspose(192, 3, 1, "valid", activation="relu"),
+            KL.Conv2DTranspose(144, 3, 1, "valid", activation="relu"),
+            KL.Conv2DTranspose(144, 3, 1, "valid", activation="relu"),
+            KL.Conv2DTranspose(96, 3, 2, "same", activation="relu"),
+            KL.Conv2DTranspose(48, 5, 2, "same", activation="relu"),
+            KL.Conv2DTranspose(24, 5, 2, "same", activation="relu"),
+            KL.Conv2DTranspose(3, 7, 2, "same"),
         ])
 
     def call(self, x, training=None):
         x = self.mlp(x, training=training)
-        x = KL.Reshape(self.feat_shape)(x)
-        x = self.decnn(x)
+        x = self.reshape(x)
+        x = self.decnn(x, training=training)
 
         return x
 
-
-class GRUDynamics(KL.Layer):
-
-    MLP_HIDDEN_UNITS = 128
-    MLP_NUM_LAYERS = 2
-    MLP_DROPOUT = None
-
-    def __init__(self,
-                 state_units: int,
-                 **kwargs):
-        super(GRUDynamics, self).__init__(**kwargs)
-        self.state_units = state_units
-        self.gru = KL.GRU(2 * state_units, activation=None)
-
-    def call(self, inputs, mask=None, training=None):
-        ctrl_inputs_bt2, init_state = inputs
-        gru_state = self.gru(
-            ctrl_inputs_bt2, initial_state=init_state, mask=mask, training=training)
-        z = gru_state[..., :self.state_units]
-        z_log_var = gru_state[..., self.state_units:]
-        return z, z_log_var
-
-class KalmanMeasure(KL.Layer):
-    def __init__(self, state_units: int, observe_units: int, **kwargs):
-        super(KalmanMeasure, self).__init__(**kwargs)
-        self.state_units = state_units
-        self.observe_units = observe_units
-
-    def build(self, input_shapes):
-        self.C = self.add_weight(
-            name="C",
-            shape=(self.observe_units, self.state_units),
-            trainable=True,
-        )
-
-    def call(self, inputs):
-        # process prediction
-        z, z_log_var, y = inputs
-        z_vec = z[..., None]
-        Pz = tf.linalg.diag(tf.exp(z_log_var))
-
-        C = self.C[None]
-        C_T = tf.linalg.adjoint(C)
-
-        y_hat_vec = C @ z_vec
-        y_hat = y_hat_vec[..., 0]
-
-        r = y - y_hat
-        S = C @ Pz @ C_T + tf.eye(self.observe_units)[None]
-        K_T = tf.linalg.cholesky_solve(S, C @ Pz)
-        K = tf.linalg.adjoint(K_T)
-
-        Py = C @ Pz @ C_T + tf.eye(self.observe_units)[None]
-
-        z_vec += K @ r[..., None]
-        Pz = (tf.eye(self.state_units)[None] - K @ C) @ Pz
-
-        # corrected measurements
-        y_vec = C @ z_vec
-
-        return {
-            "z_meas": z_vec[..., 0],
-            "z_log_var_meas": tf.math.log(tf.linalg.diag_part(Pz)),
-            "y_meas": y_vec[..., 0],
-            "y_proc": y_hat,
-            "Py_proc": Py,
-        }
 
 class DPNet:
 
@@ -184,7 +112,6 @@ class DPNet:
         state_enc_units = 32,
         state_units = 3,
         control_units = 2,
-        z_units = 256,
         mlp_dropout = None,
     )
 
@@ -197,50 +124,39 @@ class DPNet:
         self.img_encoder = ImageEncoder(self.p.img_enc_units, dropout=self.p.mlp_dropout)
         image_shape = (None,) + self.data_p.img_size + (3,)
         feat_shape = self.img_encoder.cnn.compute_output_shape(image_shape)
+        print("Image feature shape:", feat_shape)
         self.img_decoder = ImageDecoder(feat_shape[1:], dropout=self.p.mlp_dropout)
         self.state_encoder = MLP(
-            self.p.state_enc_units, 64, 1, self.p.mlp_dropout, name="state_encoder")
+            self.p.state_enc_units, 32, 2, self.p.mlp_dropout, name="state_encoder")
         self.state_decoder = MLP(
-            self.p.state_units, 64, 1, self.p.mlp_dropout, name="state_decoder")
-        self.z_init_mlp = MLP(self.p.z_units, 256, 1)
+            self.p.state_units, 32, 2, self.p.mlp_dropout, name="state_decoder")
 
-        self.dynamics = GRUDynamics(state_units=self.p.z_units)
-        self.kalman_meas = KalmanMeasure(
-            state_units=self.p.z_units,
-            observe_units=self.p.img_enc_units + self.p.state_enc_units,
-            name="kalman_meas",
-        )
+        self.dynamics = KL.GRU(units=self.p.img_enc_units + self.p.state_enc_units)
         self.model = self.build_model()
 
-    def get_init_state(self, image_bhw3, state_b3):
-        image_code = self.img_encoder(image_bhw3)
+    def encode(self, img_bhw3, state_b3):
+        img_code = self.img_encoder(img_bhw3)
         state_code = self.state_encoder(state_b3)
-        y = tf.concat([image_code, state_code], axis=-1)
+        y = KL.Concatenate()([img_code, state_code])
 
-        init_z = self.z_init_mlp(y)
-        init_z_log_var = tf.zeros_like(init_z)
-
-        return {"init_state": tf.concat([init_z, init_z_log_var], axis=-1)}
+        return y, img_code, state_code
 
     def build_model(self) -> K.Model:
         img_bhw3 = KL.Input(self.data_p.img_size + (3,), batch_size=self.data_p.batch_size)
         state_b3 = KL.Input((self.p.state_units,), batch_size=self.data_p.batch_size)
         ctrl_mask_bt = KL.Input((None,), batch_size=self.data_p.batch_size, dtype=tf.bool)
         ctrl_inputs_bt2 = KL.Input((None, 2), batch_size=self.data_p.batch_size)
-        init_state = KL.Input((self.p.z_units*2,), batch_size=self.data_p.batch_size)
+        init_state = KL.Input((self.p.img_enc_units + self.p.state_enc_units,),
+                              batch_size=self.data_p.batch_size)
 
         # process model
         num_steps = tf.shape(ctrl_mask_bt)[1]
-        z, z_log_var = self.dynamics((ctrl_inputs_bt2, init_state), mask=ctrl_mask_bt)
+        z = self.dynamics(ctrl_inputs_bt2, initial_state=init_state, mask=ctrl_mask_bt)
 
-        # observation
-        img_code = self.img_encoder(img_bhw3)
-        state_code = self.state_encoder(state_b3)
+        # encoder-decoder reconstruction
+        y, img_code, state_code = self.encode(img_bhw3, state_b3)
         img_recon = self.img_decoder(img_code)
         state_recon = self.state_decoder(state_code)
-        y = KL.Concatenate()([img_code, state_code])
-
-        corrected = self.kalman_meas((z, z_log_var, y))
 
         return K.Model(
             inputs={
@@ -251,17 +167,11 @@ class DPNet:
                 "init_state": init_state,
             },
             outputs={
-                "z_proc": z,
-                "z_log_var_proc": z_log_var,
+                "z": z,
                 "y": y,
-                "y_proc": corrected["y_proc"],
-                "Py_proc": corrected["Py_proc"],
-                "z_meas": corrected["z_meas"],
-                "z_log_var_meas": corrected["z_log_var_meas"],
                 # auxiliary outputs
                 "img_recon": img_recon,
                 "state_recon": state_recon,
-                "init_z": self.z_init_mlp(y),
             },
         )
 
